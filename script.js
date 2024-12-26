df09d6f77734817dc1c40fb00b5f31e803af33b2
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-26 06:32:07 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //your JS code here. If required.
-function getFormValue(event){
-	event.preventDefault();
-	const form =documnet.getElementById("form1");
+function displayFormInfo(){
+	
+	const form =documnet.getElementById("userform");
 
-	const firstName=form.elements["form"].value;
-	const lastName=form.elements["form"].value;
-	const phoneNumber=form.elements["form"].value;
-	const EmailID=form.elements["form"].value;
+	const firstName=form.ements["fname"].value;
+	const lastName=form.elements["lname"].value;
+	const phoneNumber=form.elements["phone"].value;
+	const EmailID=form.elements["email"].value;
 
 	const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`;
 	alert(message);
+	
 	
 }
