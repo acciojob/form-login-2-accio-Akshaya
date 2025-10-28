4cc54e104e031117237588eed1f9353062041066
Akshaya <akshayaganesh0910@gmail.com>
2025-10-28 12:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//your JS code here. If required.
 function showAlert() {
-            const firstName = document.getElementById("firstName").value;
-            const lastName = document.getElementById("lastName").value;
-            const phone = document.getElementById("phone").value;
-            const email = document.getElementById("email").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
 
-            const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
+    const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
             alert(message);
-        }
+}
