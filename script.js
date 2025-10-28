cbaadd4997baa8f7738281dc7ec4d9fc3fc2d373
Akshaya <akshayaganesh0910@gmail.com>
2025-10-28 12:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //your JS code here. If required.
-function showAlert(){
-	var firstName = document.getElementById("firstName").value;
-	var lastName = document.getElementById("lastName").value;
-	var phone = document.getElementById("phone").value;
-	var email = document.getElementById("email").value;
+function showAlert() {
+            const firstName = document.getElementById("firstName").value;
+            const lastName = document.getElementById("lastName").value;
+            const phone = document.getElementById("phone").value;
+            const email = document.getElementById("email").value;
 
-
-	 const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
-
-	alert(message);
-}
+            const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
+            alert(message);
+        }
