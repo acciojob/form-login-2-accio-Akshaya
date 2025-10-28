9bc440d66b8343e5a64f75c08d71b9845a0a7d23
Akshaya <akshayaganesh0910@gmail.com>
2025-10-28 12:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function showAlert(){
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var phone = document.getElementById("phone").value;
+	var email = document.getElementById("email").value;
+
+
+	var message = " First Name: "+ firstName +
+		           " Last Name: "+ lastName +
+		           " Phone Number: "+ phone +
+		           " Email ID: "+ email;
+
+	alert(message);
+}
